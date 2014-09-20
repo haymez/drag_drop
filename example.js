7a67c8a3ea0436497a6d23999daaf169cad7f278
James Miltenberger <jamilt13@gmail.com>
2014-09-20 22:15:48 +0000

    Added mobile support

--- example.js
+++ example.js
@@ -3,9 +3,10 @@
 var btn = document.getElementById('editButton');
 
 btn.addEventListener('click', function() {
-  var list = document.getElementsByClassName('draggable')
+  var list = document.getElementsByClassName('icon')
   for(var i = 0; i < list.length; i++) {
     list[i].classList.toggle('jiggly');
+    list[i].classList.toggle('draggable');
   }
 });
 
--- src/dragDrop.js
+++ src/dragDrop.js
@@ -4,6 +4,9 @@ var DragDrop = function(callback) {
   var dragging         = false;
   var currentSelection = null;
   var oldDropZone      = null;
+  var touchstart       = null;
+  var lastMove         = null;
+
   var drag = document.createElement('div');
   drag.className = 'placeholder draggable hidden';
   drag.id = 'drag';
@@ -20,33 +23,35 @@ var DragDrop = function(callback) {
     return false;
   }
 
-
-  // For all draggable elements
-  document.addEventListener('mousedown', function(evt) {
+  var mouseDown = function(evt) {
     var curr = evt.target;
-    if(curr.className.indexOf('draggable') == -1 ||
-      curr.className.indexOf('jiggly') == -1) return;
-    oldDropZone      = evt.target.parentElement;
+    if(curr.className.indexOf('draggable') == -1) return;
+    oldDropZone      = curr.parentElement;
     currentSelection = curr;
     dragging         = true;
-  });
 
+    if(drag.className.indexOf('hidden') > -1) drag.classList.toggle('hidden');
+    if(drag.childNodes.length == 0) drag.appendChild(currentSelection.cloneNode());
+    drag.lastChild.innerHTML = currentSelection.innerHTML;
+    drag.style.left = evt.pageX - (drag.offsetWidth/2) + 'px';
+    drag.style.top = evt.pageY - (drag.offsetHeight/2) + 'px';
+  }
 
-
-  document.addEventListener('mousemove', function(evt) {
+  var moveElement = function(evt) {
+    clearTimeout(touchstart);
+    touchstart = null;
     if(dragging) {
+      lastMove = {x: evt.pageX, y: evt.pageY};
       evt.preventDefault();
-      if(drag.className.indexOf('hidden') > -1) drag.classList.toggle('hidden');
-      if(drag.childNodes.length == 0) drag.appendChild(currentSelection.cloneNode());
-      drag.lastChild.innerHTML = currentSelection.innerHTML;
       drag.style.left = evt.pageX - (drag.offsetWidth/2) + 'px';
       drag.style.top = evt.pageY - (drag.offsetHeight/2) + 'px';
     }
-  })
-
-  document.addEventListener('mouseup', function(evt) {
+  }
+  
+  var dropElement = function(evt) {
     if(dragging) {
-      var dropped = isWithin({x: evt.pageX, y: evt.pageY})
+      var dropped = isWithin(lastMove);
+      // var dropped = isWithin({x: evt.pageX, y: evt.pageY})
       if(dropped) {
         callback.call(this, {from: oldDropZone, to: dropped, item: currentSelection})
       }
@@ -55,5 +60,31 @@ var DragDrop = function(callback) {
     drag.innerHTML = '';
     currentSelection = null;
     dragging = false;
-  }); 
+  }
+
+  var touchStart = function(evt) {
+    var curr = evt.target;
+    if(curr.className.indexOf('draggable') > -1) {
+      // Hold finger for 250 ms to make element movable
+      if(!touchstart) touchstart = setTimeout(function() {
+        mouseDown(evt);
+      }, 250);
+    }
+  }
+
+  var touchEnd = function(evt) {
+    if(dragging) {
+      dropElement(lastMove);
+      clearTimeout(touchstart);
+      touchstart = null;
+    }
+  }
+
+  // Events to listen for...
+  document.addEventListener('mousedown'  , function(evt) {  mouseDown(evt);    });
+  document.addEventListener('mousemove'  , function(evt) {  moveElement(evt);  });
+  document.addEventListener('mouseup'    , function(evt) {  dropElement(evt);  });
+  document.addEventListener('touchstart' , function(evt) {  touchStart(evt);   });
+  document.addEventListener('touchend'   , function(evt) {  touchEnd(evt);     });
+  document.addEventListener('touchmove'  , function(evt) {  moveElement(evt);  });
 }
